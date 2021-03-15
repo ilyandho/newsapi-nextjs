6eb3f4575829bd7e67de6175c6f7f61470ab30bf
ilyandho <ii.ndhote@gmail.com>
2021-03-15 21:30:39 +0300

    Navigation bar added for both mobile and desktop devices

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Header from "../components/Header/Header";
 import Head from "next/head";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <h1>NewsApi</h1>
     </div>
   );
